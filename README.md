3eccf7f2754ffd0694d9a7b7dd63c94f4c58318b
MaryamNona <>
2021-05-12 01:23:04 +0000

    added script file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ICS2O-Unit2-03-HTML-User_Input
+
+[![GitHub's Super Linter](https://github.com/MaryamNona/ICS2O-Unit2-03-HTML-User_Input/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MaryamNona/ICS2O-Unit2-03-HTML-User_Input/actions)
+
+This website can be found at: [https://MaryamNona.github.io/ICS2O-Unit2-03-HTML-User_Input](https://MaryamNona.github.io/ICS2O-Unit2-03-HTML-User_Input)
---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Maryam Nona
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets users street name ans street number and shows it back to the user
+
+  // input
+  const streetNumber = parseInt(document.getElementById('street-entered').value)
+  const streetName = document.getElementById('street-name').value
+
+  // output
+document.getElementById('address').innerHTML = 'Your info is: ' + streetNumber + ' ' + streetName + '.'
+}
